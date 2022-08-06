f841ede40e0dbb398f98606e7a729dea9bd210dd
toanlbk20504 <tung0935353639@gmail.com>
2022-08-06 23:06:39 +0700

    btvn js buoi 6

---
+++ btvn_js_buoi6.js
@@ -0,0 +1,35 @@
+//Cau 1
+function timSoNhoNhat() {
+  //  var nhapSo=document.getElementById("nhapSo").value*1;
+    var sum=null;
+    var n=1;
+    while (sum<10000) {
+        sum=sum+n;
+        n++;
+    }
+    document.getElementById("ketQua1").innerHTML=n;
+}
+//Cau2
+function tinhTong() {
+     var soX=document.getElementById("soX").value*1;
+     var soN=document.getElementById("soN").value*1;
+     var tong=null;
+     var lt=1;
+     var dem=1;
+     while (dem<=soN) {
+      lt=lt*soX;
+      tong= lt+tong;
+      dem++;
+     }
+     document.getElementById("ketQua2").innerHTML=tong;
+}
+//Cau3
+function tinhGiaiThua() {
+  var inPut3=document.getElementById("inPut3").value*1;
+  var dem=1;
+  var output=1;
+  while (dem<=inPut3) {
+      output= output*dem;
+      dem= dem+1;
+  }
+  document.getElementById("ketQua3").innerHTML=output; }
